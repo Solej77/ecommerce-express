2d68ef1add4f0a2eeda34dde0b9dc3422fd5f5cc
Solej77 <ing.josgalo@gmail.com>
2020-03-08 15:01:36 -0600

    Creating tests for our endpoints

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const passport = require("passport");
-const router = express.Router();
 const ProductsService = require('../../services/products');
 const validation = require('../../utils/middlewares/validationHandler');
 
@@ -16,94 +15,99 @@ require("../../utils/auth/strategies/jwt");
 
 const productsService = new ProductsService();
 
-router.get('/', async function(req, res, next) {
-  const { tags } = req.query;
-  console.log('req', req.query);
+// aplicación del patron inversión de control
+function productsApi(app) {
+  const router = express.Router();
+  app.use("/api/products", router);
 
-  try {
-    const products = await productsService.getProducts({ tags })
-    
-    res.status(200).json({
-      data: products,
-      message: 'products listed'
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+  router.get('/', async function(req, res, next) {
+    const { tags } = req.query;
 
-router.get('/:productId', async function(req, res, next) {
-  const { productId } = req.params;
-  console.log('productId: ', productId)
-  console.log('req', req.params);
-
-  try {
-    const product = await productsService.getProduct({ productId });
-    
-    res.status(200).json({
-      data: product,
-      message: 'products retrieved'
-    });
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post('/', validation(createProductSchema), async function(req, res, next) {
-  const { body: product } = req;
-
-  try {
-    const createdProduct = await productsService.createProduct({ product });
-
-    res.status(201).json({
-      data: createdProduct,
-      message: 'product created'
-    });
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.put(
-  '/:productId',
-  passport.authenticate("jwt", { session: false }),
-  validation({ productId: productIdSchema }, "params"),
-  validation(updateProductSchema),
-  async function(req, res, next) {
-    const { productId } = req.params;
-    const product = req.body;
-    console.log('req', req.params);
-    console.log('body', product);
-  
     try {
-      const updatedProduct = await productsService.updateProduct({ productId, product });
+      const products = await productsService.getProducts({ tags })
+      
+      res.status(200).json({
+        data: products,
+        message: 'products listed'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.get('/:productId', async function(req, res, next) {
+    const { productId } = req.params;
+    console.log('productId: ', productId)
+    console.log('req', req.params);
+
+    try {
+      const product = await productsService.getProduct({ productId });
+      
+      res.status(200).json({
+        data: product,
+        message: 'products retrieved'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.post('/', validation(createProductSchema), async function(req, res, next) {
+    const { body: product } = req;
+
+    try {
+      const createdProduct = await productsService.createProduct({ product });
 
       res.status(201).json({
-        data: updatedProduct,
-        message: 'product updated'
+        data: createdProduct,
+        message: 'product created'
       });
     } catch (err) {
       next(err);
     }
-});
+  });
 
-router.delete(
-  '/:productId',
-  passport.authenticate("jwt", { session: false }),
-  async function(req, res, next){
-    const { productId } = req.params;
-    console.log('req', req.params);
-
-    try {
-      const deletedProduct = await productsService.deleteProduct({ productId });
+  router.put(
+    '/:productId',
+    passport.authenticate("jwt", { session: false }),
+    validation({ productId: productIdSchema }, "params"),
+    validation(updateProductSchema),
+    async function(req, res, next) {
+      const { productId } = req.params;
+      const product = req.body;
+      console.log('req', req.params);
+      console.log('body', product);
     
-      res.status(200).json({
-        data: deletedProduct,
-        message: 'product deleted'
-      });
-    } catch (err) {
-      next(err);
-    }
-});
+      try {
+        const updatedProduct = await productsService.updateProduct({ productId, product });
 
-module.exports = router;
+        res.status(201).json({
+          data: updatedProduct,
+          message: 'product updated'
+        });
+      } catch (err) {
+        next(err);
+      }
+  });
+
+  router.delete(
+    '/:productId',
+    passport.authenticate("jwt", { session: false }),
+    async function(req, res, next){
+      const { productId } = req.params;
+      console.log('req', req.params);
+
+      try {
+        const deletedProduct = await productsService.deleteProduct({ productId });
+      
+        res.status(200).json({
+          data: deletedProduct,
+          message: 'product deleted'
+        });
+      } catch (err) {
+        next(err);
+      }
+  });
+}
+
+module.exports = productsApi;
--- utils/mocks/products.js
+++ utils/mocks/products.js
@@ -1,14 +1,46 @@
-const productMocks = [
+const productsMock = [
+  {
+    name: "Blue sneakers",
+    price: 75,
+    image: "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0e44599932dce6b8440e26fb91e10a69&auto=format&fit=crop&w=800&q=60",
+    tags: ["red", "expensive"],
+  },
+  {
+    name: "White bike",
+    price: 300,
+    image: "https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=53d820e6622fadd53b8638d60f468ccd&auto=format&fit=crop&w=800&q=60",
+    tags: ["black", "expensive"],
+  },
   {
     name: "Red shoes",
     price: 75,
-    image: "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0e44599932dce6b8440e26fb91e10a69&auto=format&fit=crop&w=800&q=60"
+    image: "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0e44599932dce6b8440e26fb91e10a69&auto=format&fit=crop&w=800&q=60",
+    tags: ["red", "cheap"],
   },
   {
     name: "Black bike",
     price: 300,
-    image: "https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=53d820e6622fadd53b8638d60f468ccd&auto=format&fit=crop&w=800&q=60"
+    image: "https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=53d820e6622fadd53b8638d60f468ccd&auto=format&fit=crop&w=800&q=60",
+    tags: ["black", "cheap"],
   }
 ];
 
-module.exports = productMocks;
+function filteredProductsMocks(tags){
+  return productsMock.filter(product => product.tags.includes(tag));
+}
+
+class ProductsServiceMock {
+  async getProducts() {
+    return Promise.resolve(productsMock);
+  }
+
+  async createProduct() {
+    return Promise.resolve("5e64535b5dcf8c0885a2169f");
+  }
+}
+
+module.exports = {
+  productsMock,
+  filteredProductsMocks,
+  ProductsServiceMock,
+};
